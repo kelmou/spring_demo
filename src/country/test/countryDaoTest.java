45bda2cdbf1d4f101e816033fd4543938d878138
Leila KELMOUA <leilakelmoua@gmail.com>
2022-01-20 21:01:06 +0100

    Supprimer un pays

--- src/country/test/countryDaoTest.java
+++ src/country/test/countryDaoTest.java
@@ -19,7 +19,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
 
-public class countryDaoTest   {
+public class CountryDaoTest   {
 	@Autowired
 	CountryDAO countryDao;
 	List<Country>countries=new ArrayList<Country>();
